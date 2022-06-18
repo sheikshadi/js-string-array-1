9baf6fa20853e23f9cb80a28da0c2319f914dfa3
sheikshadi <sheikshadi2022@gmail.com>
2022-06-18 15:05:58 +0600

    hello

---
+++ case-convert.js
@@ -0,0 +1,10 @@
+const anthem = 'amar Sonar Bangla Ami Tomai Valoabasi';
+const search = 'valo';
+
+const userInput = 'BlACkkPink';
+const savedUserName = 'blacKKPink';
+// console.log(userInput.toLowerCase());
+// console.log(savedUserName.toLowerCase());
+if(userInput.toLowerCase() == savedUserName.toLowerCase()){
+    console.log('user exsist');
+}
